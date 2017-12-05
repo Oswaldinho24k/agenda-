bf0904005ed6a51eac56632cec15a5b1b7f98570
Oswaldinho <oswalfut_96@hotmail.com>
2017-12-05 15:53:51 -0600

    added material and the router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {MuiThemeProvider} from "material-ui";
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+const WithRouter = () => (
+
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+
+);
+
+const Main = ()=>(
+    <MuiThemeProvider muiTheme={muiTheme}>
+        <Provider store={store}>
+            <WithRouter/>
+        </Provider>
+    </MuiThemeProvider>
+);
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
+

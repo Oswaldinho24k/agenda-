33ec1c2e1710dda9364a6c942538a2324c3c6018
Sammelot <stolzblut@gmail.com>
2018-02-06 14:16:20 -0600

    Calendar

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import LoginContainer from './components/login/LoginContainer';
 import PrincipalContainer from './components/principal/PrincipalContainer';
+import CalendarioContainer from './components/calendario/CalendarioContainer';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/login" component={LoginContainer}/>
         <Route path="/agenda" component={PrincipalContainer}/>
+        <Route path="/calendario" component={CalendarioContainer} />
     </Switch>
 
 );
---
+++ src/components/calendario/CalendarContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CalendarContainer extends React.Component{
+    render(){
+        return(
+
+        );
+    }
+}
+
+export default CalendarContainer;

6286a4ec831f559d56f3fc9513ad4e13687a8c87
Sammelot <stolzblut@gmail.com>
2018-02-07 17:19:52 -0600

    CAlendario con eventos d&d

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,12 +3,15 @@ import * as $ from 'jquery';
 import 'fullcalendar';
 import '../../../node_modules/fullcalendar/dist/fullcalendar.css';
 import '../../../node_modules/jquery/dist/jquery.js';
+import '../../../node_modules/jquery/dist/jquery.min';
+import '../../../node_modules/jquery-ui-dist/jquery-ui.min';
 import '../../../node_modules/moment/moment.js';
 
 class Application extends React.Component {
     render() {
         return (
             <div>
+                <External/>
                 <Calendar/>
             </div>
         );
@@ -42,6 +45,40 @@ class Calendar extends React.Component {
                 }
             }
         })
+    }
+}
+
+class External extends React.Component {
+    render() {
+        return <div id='external-events'>
+            <h4>Draggable Events</h4>
+            <div className='fc-event'>My Event 1</div>
+            <div className='fc-event'>My Event 2</div>
+            <div className='fc-event'>My Event 3</div>
+            <div className='fc-event'>My Event 4</div>
+            <div className='fc-event'>My Event 5</div>
+            <p>
+                <input type='checkbox' id='drop-remove' />
+                <label for='drop-remove'>remove after drop</label>
+            </p>
+        </div>;
+    }
+    componentDidMount() {
+        $('#external-events .fc-event').each(function() {
+
+            // store data so the calendar knows to render an event upon drop
+            $(this).data('event', {
+                title: $.trim($(this).text()), // use the element's text as the event title
+                stick: true // maintain when user navigates (see docs on the renderEvent method)
+            });
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999,
+                revert: true,      // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
+            });
+        });
     }
 }
 

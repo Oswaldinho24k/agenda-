9e675ad2593489913f8e6b7f9c3d49025aae9a8c
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-02-28 02:04:29 -0600

    dash

--- src/components/employees/EmployeesComponents.js
+++ src/components/employees/EmployeesComponents.js
@@ -24,7 +24,7 @@ const EmployeesComponents = ({userAll,openDelete,openEdit})=>{
               enableSelectAll={true}
                >
                <TableRow>
-                 <TableHeaderColumn>User</TableHeaderColumn>
+                 <TableHeaderColumn>Nombre</TableHeaderColumn>
                  <TableHeaderColumn>Email</TableHeaderColumn>
                  <TableHeaderColumn>Puesto</TableHeaderColumn>
                  <TableHeaderColumn>Detalle </TableHeaderColumn>
